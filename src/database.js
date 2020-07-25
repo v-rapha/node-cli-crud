b26874387601384e1f1996d01d4da02c519b5cff
vrapha <vs.raphael19@gmail.com>
2020-07-25 13:43:55 -0300

    create dev using tests

--- src/database.js
+++ src/database.js
@@ -1,7 +1,8 @@
-const { readFile } = require('fs');
+const { readFile, writeFile } = require('fs');
 const { promisify } = require('util');
 
 const readFileAsync = promisify(readFile);
+const writeFileAsync = promisify(writeFile);
 
 class Database {
   constructor() {
@@ -18,13 +19,39 @@ class Database {
     }
   }
 
-  writeFile() {}
+  async writeInFile(data) {
+    try {
+      await writeFileAsync(this.FILE_PATH, JSON.stringify(data));
+      return true;
+    } catch (error) {
+      console.error('writeInFile - ERROR', error);
+    }
+  }
 
   async read(id) {
-    const data = await this.getFileData();
-    const filteredData = data.filter(item => (id ? item.id === id : true));
+    try {
+      const data = await this.getFileData();
+      const filteredData = data.filter(item => (id ? item.id === id : true));
 
-    return filteredData;
+      return filteredData;
+    } catch (error) {
+      console.error('read - ERROR', error);
+    }
+  }
+
+  async create(dev) {
+    try {
+      const developersList = await this.getFileData();
+      const id = dev.id <= 2 ? dev.id : Date.now();
+
+      const setDeveloperId = { id, ...dev };
+      const setDevelopersList = [...developersList, setDeveloperId];
+
+      const result = await this.writeInFile(setDevelopersList);
+      return result;
+    } catch (error) {
+      console.error('create - ERROR', error);
+    }
   }
 }
 
--- src/test.js
+++ src/test.js
@@ -1,4 +1,4 @@
-const { deepStrictEqual } = require('assert');
+const { deepStrictEqual, ok } = require('assert');
 const Database = require('./database');
 
 const MOCK_DEV_REGISTER = {
@@ -6,9 +6,13 @@ const MOCK_DEV_REGISTER = {
   name: 'v-rapha',
   hole: 'back-end',
   level: 'junior',
-}
+};
 
 describe('Developer manipulation suite', function () {
+  this.beforeAll(async () => {
+    await Database.create(MOCK_DEV_REGISTER);
+  });
+
   it('should search for a developer by id using files', async () => {
     const expected = MOCK_DEV_REGISTER;
     const [result] = await Database.read(expected.id);
@@ -16,7 +20,12 @@ describe('Developer manipulation suite', function () {
     deepStrictEqual(result, expected);
   });
 
-  // it('should register a developer using files', () => {
-  //   deepStrictEqual(2, 2);
-  // });
+  it('should register a developer using files', async () => {
+    const expected = MOCK_DEV_REGISTER;
+    const result = await Database.create(MOCK_DEV_REGISTER);
+    const [current] = await Database.read(MOCK_DEV_REGISTER.id);
+
+    deepStrictEqual(current, expected);
+    ok(result === true);
+  });
 });
